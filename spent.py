68b770cb36913e79507a217eece9696f38292bb2
root <blair@blairjames.com>
2018-07-18 21:22:07 +1000

    added banner

--- spent.py
+++ spent.py
@@ -10,6 +10,7 @@ class Spencer:
     def __init__(self):
         self.domain: str = "https://qed.qld.gov.au/"
         self.wordlist_path: str = os.getcwd() + "/sharepoint_list.txt"
+        self.log_path: str = os.getcwd() + "/last_log.txt"
 
     async def read_list(self) -> Generator:
         file = open(self.wordlist_path, "r")
@@ -45,17 +46,34 @@ class Spencer:
     async def controller(self):
         try:
             finish = await self.launch_reqests()
-            [print(f) for f in finish if not
-            "<p>HTTP Error 400. The request URL is invalid.</p>" in f]
+            [print(f) for f in finish]
+             #if not "<p>HTTP Error 400. The request URL is invalid.</p>" in f]
 
         except Exception as e:
             print("parse_response: " + str(e))
 
+    def clear_log_file(self):
+        try:
+            with open(self.log_path, "w") as file:
+                file.write("")
+        except Exception as e:
+            print("Error! in clear_log_file: " + str(e))
+
+    def present(self):
+        os.system("clear")
+        self.clear_log_file()
+        print(" ___  ___  ___  _ _  ___ ")
+        print("/ __>| . \| __>| \ ||_ _|")
+        print("\__ \|  _/| _> |   | | | ")
+        print("<___/|_|  |___>|_\_| |_| ")
+        print("\nSharePoint ENumeration Tool")
+        print("\nProcessing requests..")
 
 def main():
     try:
         spen = Spencer()
         loop = asyncio.get_event_loop()
+        spen.present()
         task = loop.create_task(spen.controller())
         loop.run_until_complete(task)
     except Exception as e:
